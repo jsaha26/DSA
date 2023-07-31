22ff9a7016aced7647de43e915df87faf4db5005
jsaha26 <73419909+jsaha26@users.noreply.github.com>
2023-07-31 20:09:25 +0530

    Create Product of Array Except Self.py

---
+++ Arrays/Product of Array Except Self.py
@@ -0,0 +1,17 @@
+class Solution:
+    def productExceptSelf(self, nums: List[int]) -> List[int]:
+        n = len(nums)
+        ans = [1] * n
+
+        # Calculate the product of elements to the left of the current element
+        for i in range(1, n):
+            ans[i] = ans[i-1] * nums[i-1]
+
+        right_product = 1
+
+        # Calculate the product of elements to the right of the current element
+        for i in range(n-1, -1, -1):
+            ans[i] *= right_product
+            right_product *= nums[i]
+
+        return ans
