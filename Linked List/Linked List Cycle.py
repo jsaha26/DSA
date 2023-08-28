458af8ae319565f027c56297e5259a78fc519eb1
jsaha26 <73419909+jsaha26@users.noreply.github.com>
2023-08-29 02:01:59 +0530

    Create Linked List Cycle.py

---
+++ Linked List/Linked List Cycle.py
@@ -0,0 +1,21 @@
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution(object):
+    def hasCycle(self, head):
+        """
+        :type head: ListNode
+        :rtype: bool
+        """
+        seen = set()
+        curr = head
+        while curr:
+            if curr in seen:
+                return True
+            seen.add(curr)
+            curr.next
+        return False
+        
