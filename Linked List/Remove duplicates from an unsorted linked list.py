532a2b5a317aeacccf3c4d2c9d2577389fb24366
jsaha26 <73419909+jsaha26@users.noreply.github.com>
2023-08-31 12:52:15 +0530

    Create Remove duplicates from an unsorted linked list.py

---
+++ Linked List/Remove duplicates from an unsorted linked list.py
@@ -0,0 +1,27 @@
+#https://practice.geeksforgeeks.org/problems/remove-duplicates-from-an-unsorted-linked-list/
+
+#User function Template for python3
+'''
+		# Node Class
+		class Node:
+		    def __init__(self, data):   # data -> value stored in node
+		        self.data = data
+		        self.next = None
+	
+'''
+class Solution:
+    def removeDuplicates(self, head):
+
+        seen = set()
+        curr = head
+        prev = None
+        
+        while curr:
+            if curr.data in seen:
+                prev.next = curr.next
+            else:
+                seen.add(curr.data)
+                prev = curr
+            curr = curr.next
+        
+        return head
