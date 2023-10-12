8a9f9a13f56ca57567bda19bed82884274a4003b
jsaha26 <73419909+jsaha26@users.noreply.github.com>
2023-10-12 11:28:29 +0530

    Create Group Anagrams.py

---
+++ Strings/Group Anagrams.py
@@ -0,0 +1,10 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        dic = {}
+        for i in strs:
+            w = ''.join(sorted(i))
+            if w in dic:
+                dic[w].append(i)
+            else:
+                dic[w] = [i]
+        return list(dic.values())
