64922d50462f5ec5b9ea2cedfc9dc9e69cbd6c0e
jsaha26 <73419909+jsaha26@users.noreply.github.com>
2023-10-18 16:56:52 +0530

    Create Find the Index of the First Occurrence in a String (Sliding Window).py

---
+++ Strings/Find the Index of the First Occurrence in a String (Sliding Window).py
@@ -0,0 +1,9 @@
+class Solution(object):
+    def strStr(self, haystack, needle):
+        l,r = 0,len(needle)
+        while r <= len(haystack):
+            if haystack[l:r] == needle:
+                return l
+            l += 1
+            r += 1
+        return -1
