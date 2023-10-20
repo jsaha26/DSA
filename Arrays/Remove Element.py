281806e9293f8235eeef47c2c44e6388c48bac26
jsaha26 <73419909+jsaha26@users.noreply.github.com>
2023-10-21 02:12:58 +0530

    Update Remove Element.py

--- Arrays/Remove Element.py
+++ Arrays/Remove Element.py
@@ -1,1 +1,6 @@
-#
+#https://leetcode.com/problems/remove-element/
+
+class Solution(object):
+    def removeElement(self, nums, val):
+        nums[:] = [x for x in nums if x != val]
+        return len(nums)
