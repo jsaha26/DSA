896195f1f73af7f55caf1a0b6de680a694d56302
jsaha26 <73419909+jsaha26@users.noreply.github.com>
2023-10-25 16:34:00 +0530

    Update and rename Intersection of Two Arrays.py to Arrays/Intersection of Two Arrays.py

--- Arrays/Intersection of Two Arrays.py
+++ Arrays/Intersection of Two Arrays.py
@@ -1,3 +1,5 @@
+#https://leetcode.com/problems/intersection-of-two-arrays/
+
 class Solution(object):
     def intersection(self, nums1, nums2):
         return set([x for x in nums1 if x in nums2])
