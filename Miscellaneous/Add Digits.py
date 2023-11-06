151823f13ab9d0212ede7e72608bc2d1245e07b9
jsaha26 <73419909+jsaha26@users.noreply.github.com>
2023-11-06 23:54:55 +0530

    Create Add Digits.py

---
+++ Miscellaneous/Add Digits.py
@@ -0,0 +1,11 @@
+# https://leetcode.com/problems/add-digits/submissions/1092953676/
+
+class Solution:
+    def addDigits(self, num: int) -> int:
+        while num > 9:
+            sum = 0
+            while num:
+                sum += num%10
+                num = num//10
+            num = sum
+        return num
