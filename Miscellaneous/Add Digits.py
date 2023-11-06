aa0a45cbe9ec884e12c22989ad7c60b0c951b57e
jsaha26 <73419909+jsaha26@users.noreply.github.com>
2023-11-06 23:55:39 +0530

    Update Add Digits.py

--- Miscellaneous/Add Digits.py
+++ Miscellaneous/Add Digits.py
@@ -6,6 +6,6 @@ class Solution:
             sum = 0
             while num:
                 sum += num%10
-                num = num//10
+                num //= 10
             num = sum
         return num
