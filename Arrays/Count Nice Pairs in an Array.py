1cda4d02bbce9e3223be3a7cce313dca95efd124
jsaha26 <73419909+jsaha26@users.noreply.github.com>
2023-11-21 18:05:05 +0530

    Create Count Nice Pairs in an Array.py

---
+++ Arrays/Count Nice Pairs in an Array.py
@@ -0,0 +1,20 @@
+class Solution:
+    def countNicePairs(self, nums: List[int]) -> int:
+
+        def reverse_num(x):
+            return int(str(x)[::-1])
+
+        mod = 10**9 + 7
+        seen = {}
+        nice_pairs = 0
+
+        for i in range(len(nums)):
+            rev_i = reverse_num(nums[i])
+            diff = nums[i] - rev_i
+
+            if diff in seen:
+                nice_pairs = (nice_pairs + seen[diff]) % mod
+
+            seen[diff] = (seen.get(diff, 0) + 1) % mod
+
+        return nice_pairs
