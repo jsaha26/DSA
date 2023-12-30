409eae06d61f729378b09c4764c1bc137aa60573
jsaha26 <73419909+jsaha26@users.noreply.github.com>
2023-12-30 23:44:16 +0530

    Update Contains Duplicate.py

--- Arrays/Contains Duplicate.py
+++ Arrays/Contains Duplicate.py
@@ -1,4 +1,5 @@
-#Contains Duplicate
+# https://leetcode.com/problems/contains-duplicate/
+
 class Solution:
     def containsDuplicate(self, nums: List[int]) -> bool:
         if len(set(nums)) != len(nums):
