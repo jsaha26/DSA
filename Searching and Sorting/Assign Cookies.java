2ca4867e58110dabb4fdc8189dd254e7ec448f98
jsaha26 <73419909+jsaha26@users.noreply.github.com>
2024-01-02 01:36:03 +0530

    Create Assign Cookies.java

---
+++ Searching and Sorting/Assign Cookies.java
@@ -0,0 +1,12 @@
+// https://leetcode.com/problems/assign-cookies/
+
+class Solution {
+    public int findContentChildren(int[] g, int[] s) {
+        Arrays.sort(g);
+        Arrays.sort(s);
+        int i = 0;
+        for(int j=0;i<g.length && j<s.length;j++)
+	        if(g[i]<=s[j]) i++;
+        return i;
+    }
+}
