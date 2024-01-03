0fcbc39c6060c97a59e0dd3758e4d12d9532d95d
jsaha26 <73419909+jsaha26@users.noreply.github.com>
2024-01-04 02:42:08 +0530

    Update Number of Laser Beams in a Bank.py

--- Greedy/Number of Laser Beams in a Bank.py
+++ Greedy/Number of Laser Beams in a Bank.py
@@ -1,19 +1,10 @@
 # https://leetcode.com/problems/number-of-laser-beams-in-a-bank/
 
 class Solution:
-    def maxProduct(self, nums: List[int]) -> int:
-        res = max(nums)
-        currmax, currmin = 1,1
-
-        for n in nums:
-
-            if n == 0:
-                currmax, currmin = 1,1
-
-            temp = currmax * n
-
-            currmax = max(temp, n*currmin, n)
-            currmin = min(temp, n*currmin, n)
-            res = max(currmax, res)
-
+    def numberOfBeams(self, bank: List[str]) -> int:
+        prev,res = bank[0].count("1"),0
+        for i in range(1,len(bank)):
+            if ones:= bank[i].count("1"):
+                res += (ones*prev)
+                prev = ones
         return res
